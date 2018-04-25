98af737547778b301d00ac76636459fbf600e224
vince.su <vacvin@gmail.com>
2018-04-25 11:00:55 +0800

    Collect all SQL Methods to DBService.py.

--- index.py
+++ index.py
@@ -1,65 +1,16 @@
-import sqlite3
-conn = sqlite3.connect('UserData.db')
-tableName = 'User'
-c = conn.cursor()
+import DBService 
 
-#region DB Control
-def checkTableReady():
-    sqlstr = "SELECT name FROM sqlite_master WHERE type='table' AND name='{table}';".format(table = tableName)
-    c.execute(sqlstr)
-    result = c.fetchone()
-    if result == None:
-        sqlstr = "CREATE TABLE {table} (id int, name text, birthday text, createTime text);".format(table = tableName)
-        c.execute(sqlstr)
+DBService.DBConnect("UserData.db", "User")
+DBService.checkTableReady()
 
-def getMaxId():
-    maxId = 0
-    sqlstr = "SELECT max(id) FROM {table};".format(table = tableName)
-    c.execute(sqlstr)
-    result = c.fetchone()
-    if len(result) > 0:
-        if result[0] == None:
-            return 0
-        else:
-            return int(result[0])
-    return maxId
+#DBService.insert("test", "2000-01-01")
 
-def select_All():
-    sqlstr = "SELECT * FROM {table};".format(table = tableName)
-    c.execute(sqlstr)
-    return c.fetchall()
+#DBService.update(2, "test2", "2001-01-01")
 
-def select_By_ID(id):
-    sqlstr = "SELECT * FROM {table} WHERE id = ?;".format(table = tableName)
-    c.execute(sqlstr, str(id))
-    return c.fetchone()
+#DBService.delete(2)
 
-def insert(name, birthday):
-    sqlstr = "INSERT INTO {table} VALUES (?, ?, ?, date('now'))".format(table = tableName)
-    c.execute(sqlstr, (getMaxId() + 1, name, birthday))
-    conn.commit()
-
-def update(id, name, birthday):
-    sqlstr = "UPDATE {table} SET name = ?, birthday = ? WHERE id = ?;".format(table = tableName)
-    c.execute(sqlstr, (name, birthday, id))
-    return c.fetchone()
-
-def delete(id):
-    sqlstr = "DELETE FROM {table} WHERE id = ?;".format(table = tableName)
-    c.execute(sqlstr, str(id))
-    conn.commit()
-#endregion
-
-checkTableReady()
-
-#insert("test", "2000-01-01")
-
-#update(2, "test2", "2001-01-01")
-
-#delete(2)
-
-#selectResult = select_By_ID(3)
-selectResult = select_All()
+#selectResult = DBService.select_By_ID(3)
+selectResult = DBService.select_All()
 print(selectResult)
 
-conn.close()
+DBService.DBClose()
